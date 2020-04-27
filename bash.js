d7f05dce506a590bdd1af19a4448b66380fa3eec
Javier Montero <monterodotjavier@icloud.com>
2020-04-27 13:14:15 -0400

    added pwd.js file

---
+++ bash.js
@@ -0,0 +1,13 @@
+// Output a prompt
+process.stdout.write("prompt > ");
+
+// The stdin 'data" event fires after a user types in a line
+process.stdin.on("data", (data) => {
+  // const cmd = data.toString().trim(); // remove the newline
+
+  // process.stdout.write(cmd);
+
+  // Calls the entire directory
+  process.stdout.write("Current Working Directory: " + process.cwd());
+  process.stdout.write("\nprompt > ");
+});
